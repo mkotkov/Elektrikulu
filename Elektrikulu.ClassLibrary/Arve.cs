fe2dac808c53a009651178275283cb09a94215fd
mkotkov <maximkotkov@gmail.com>
2026-09-10 17:56:14 +0300

    Add project files.

---
+++ Elektrikulu.ClassLibrary/Arve.cs
@@ -0,0 +1,20 @@
+﻿namespace Elektrikulu.ClassLibrary
+{
+    public class Arve
+    {
+       static void Main(string[] args)
+       {
+       }
+
+        public static decimal Arve_lugemine(decimal kogus, decimal hind, decimal kaibemaksu_protsent, bool kaibemaksu_kasutamine)
+        {
+            decimal kaibemaksu_summa = (hind * kogus) * (kaibemaksu_protsent / 100);
+            decimal kogusumma = (hind * kogus);
+            if (kaibemaksu_kasutamine)
+            {
+                kogusumma += kaibemaksu_summa;
+            }
+            return kogusumma;
+        }
+    }
+}
---
+++ Elektrikulu.WpfApp/MainWindow.xaml.cs
@@ -0,0 +1,20 @@
+﻿using System.Windows;
+
+namespace Elektrikulu.WpfApp
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Kàibemaksu_Checked(object sender, RoutedEventArgs e)
+        {
+
+        }
+    }
+}
